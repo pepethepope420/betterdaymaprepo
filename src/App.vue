a4b994ba809ee858ea79417983bdda631d67f6b8
Extra <67444448+pepethepope420@users.noreply.github.com>
2024-09-18 09:52:18 +0930

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import posts from './assets/posts.json'
+</script>
+
+<template>
+  <header>
+    <h1>Better Daymap</h1>
+    <button>add post</button>
+  </header>
+
+  <main>
+    <h2>posts:</h2>
+    <div v-for="post in posts" class="postClass">
+      <h1>{{ post.title }}</h1>
+      <h2>{{ post.author }}</h2>
+      <p>{{ post.text }}</p>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.postClass {
+  margin: 10px;
+  padding: 10px;
+  border-style: solid;
+  border-color: whitesmoke;
+}
+</style>
