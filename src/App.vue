0f1b12c572a88153d3ba8b9ca789985b704a64cb
Extra <67444448+pepethepope420@users.noreply.github.com>
2024-09-18 11:06:05 +0930

    fixed it
    weird issues

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,19 @@
   <header>
     <h1>Better Daymap</h1>
     <button>add post</button>
-    <button v-on:click="">login</button>
+    <button v-on:click="posts.toggle(); login.toggle()">login</button>
   </header>
 
   <main>
-    <postSection></postSection>
-    <loginPage></loginPage>
+    <postSection id="posts"></postSection>
+    <loginPage id="login"></loginPage>
   </main>
 </template>
 
-<script>
+<script setup>
 import loginPage from './components/login-page.vue'
 import postSection from './components/post-Section.vue'
+
 </script>
 <style scoped>
 .postClass {
--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+const mountedApp = app.mount('#app');
