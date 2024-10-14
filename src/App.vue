d11da391d57f0bb251516ce8d612f0b709e7009f
Extra <67444448+pepethepope420@users.noreply.github.com>
2024-10-14 15:25:15 +1030

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -7,27 +7,46 @@
   </header>
 
   <main>
-    <postSection v-if="openLogin == false && postPageAdd == false"></postSection>
+    <div v-if="openLogin == false && postPageAdd == false">
+        <h2>posts:</h2>
+        <div v-for="(post, i) in this.posts" v-bind:key="i">
+          <h1>{{ post.title }}</h1>
+          <h2>{{ post.author }}</h2>
+          <p>{{ post.text }}</p>
+        </div>
+      </div>
     <loginPage v-if="openLogin == true"></loginPage>
-    <addPostPage v-if="postPageAdd == true"></addPostPage>
+    <div v-if="postPageAdd == true">
+      <h2>Add Post:</h2>
+      <p>Title</p>
+      <input type="text" v-model="title">
+      <p>Tags (separate each tag by a commar)</p>
+      <input type="text" v-model="tags">
+      <p>body</p>
+      <input type="text" v-model="body">
+      <p></p>
+      <button v-on:click="addPostButton()">Post</button>
+    </div>
   </main>
 </template>
 
 <script>
+import postData from './assets/posts.json'
 import loginPage from './components/login-page.vue'
-import postSection from './components/post-Section.vue'
-import addPostPage from './components/add-post.vue'
 export default {
   name: "app",
   components: {
       loginPage,
-      postSection,
-      addPostPage
+      postData
   },
   data (){
     return {
       openLogin: false,
-      postPageAdd: false
+      postPageAdd: false,
+      title:"",
+      tags:"",
+      body:"", 
+      posts: postData
     }
   },
   methods:{
@@ -36,6 +55,19 @@ export default {
     },
     addPost (){
       this.postPageAdd = true
+    },
+    addPostButton () {
+        var postCount = 1;
+        for (var post in this.posts){
+            postCount +=1;
+        }
+        var newPostName = "post" +postCount;
+        this.posts[newPostName] = {
+            "title": this.title,
+            "text": this.body,
+            "author": "testAuthor"
+        }
+        this.postPageAdd = false;
     }
   }
 }
